1b3f9ca2c12838f19d6fba57f2188cc1e1935f24
Nauja <jeremymorosi@hotmail.fr>
2023-09-15 05:51:19 +0000

    Deploying to gh-pages from @ moroboxai/moroboxai.com@bf076874874732ecb525923a6508b41ff6e558fb 🚀

--- src/redux/dispatchers/index.ts
+++
@@ -1,6 +0,0 @@
-import { Dispatch } from "redux";
-import { Actions } from "../actions/types"
-import { IGamesDb, gamesDbLoaded, selectGame } from "../actions";
-
-export const dispatchGamesDbLoaded = (dispatch: Dispatch<Actions>) => (db: IGamesDb) => dispatch(gamesDbLoaded(db));
-export const dispatchSelectGame = (dispatch: Dispatch<Actions>) => (id: string) => dispatch(selectGame(id));
