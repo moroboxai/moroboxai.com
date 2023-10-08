ca332780fd0e0389cb93889ff24b2f6c421a9cd4
Nauja <jeremymorosi@hotmail.fr>
2023-10-08 20:00:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,5 @@ const nextConfig = {
     output: "export", // Outputs a Single-Page Application (SPA).
     distDir: "./dist" // Changes the build output directory to `./dist/`.
 };
+
+module.exports = nextConfig;
