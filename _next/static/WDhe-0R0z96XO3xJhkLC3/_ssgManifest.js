33e7bda56246f84fbe47814a7fa9332474f89e51
Nauja <jeremymorosi@hotmail.fr>
2023-10-15 14:24:44 +0000

    Deploying to gh-pages from @ moroboxai/moroboxai.github.io@9dd1b0123a07448217acefb21f7c0b7977aa1b7c 🚀

---
+++ _next/static/WDhe-0R0z96XO3xJhkLC3/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fembed\u002Fgame\u002F[id]","\u002Fgame\u002F[id]","\u002Fgames","\u002Flearn\u002F[[...slug]]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
